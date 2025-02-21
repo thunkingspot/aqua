1d708bf91c3abeafd5acb6731208a51d1be02801
rich <dev@thunkingspot.com>
2025-02-21 00:31:09 +0000

    test pipeline errors

--- mgt/deploy.sh
+++ mgt/deploy.sh
@@ -5,11 +5,11 @@ if [ "$1" == "false" ]; then
   DEBUG_MODE=false
 fi
 
-sudo docker build -t aqua-app ..
+sudo docker build -t aqua-app .
 
 if [ "$DEBUG_MODE" == "true" ]; then
   sudo docker run -d -p 80:80 -p 5678:5678 -e DEBUG_MODE=true aqua-app
     else
-  sudo docker run -d -p 80:80 -p 5678:5678 -e DEBUG_MODE=false aqua-app
+  sudo docker run -d -p 80:80 -p 5678:5678 -e DEBUG_MODE=false aqua-apple
     fi
 sudo docker ps
